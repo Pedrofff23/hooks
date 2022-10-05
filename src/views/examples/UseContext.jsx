bda10ca2621d86caee60a5dae833b387ed6fbad5
Pedro Santana Mendes <pmendes09@hotmail.com>
2022-10-05 17:08:07 -0300

    class 74 UseContext

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,33 +1,62 @@
-import React, { useContext } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useContext } from "react";
+import { useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-import DataContext from '../../data/DataContext'
+import { AppContext } from "../../data/Store";
+import DataContext from "../../data/DataContext";
 
 const UseContext = (props) => {
-    const {state, setState} = useContext(DataContext)
+  const { state, setState } = useContext(DataContext);
 
-    function addNumber(n) {
-        setState({
-            ...state,
-            number: state.number + n
-        })
+  function addNumber(n) {
+    setState({
+      ...state,
+      number: state.number + n,
+    });
+  }
+  const { number, setNumber, text, setText } = useContext(AppContext);
+  useEffect(function () {
+    if(number>1250){
+        setText('Eita!!!!')
     }
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
-            <div className="center">
-                <span className="text">{state.text}</span>
-                <span className="text">{state.number}</span>
-                <div>
-                    <button className="btn" onClick={() => addNumber(-1)}>-1</button>
-                    <button className="btn" onClick={() => addNumber(+1)}>+1</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+  },[number])
 
-export default UseContext
+  return (
+    <div className="UseContext">
+      <PageTitle
+        title="Hook UseContext"
+        subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <span className="text">{state.text}</span>
+        <span className="text">{state.number}</span>
+        <div>
+          <button className="btn" onClick={() => addNumber(-1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => addNumber(+1)}>
+            +1
+          </button>
+        </div>
+      </div>
+
+      <SectionTitle title="Exercicio #02" />
+      <div className="center">
+        <span className="text">{text}</span>
+        <span className="text">{number}</span>
+        <div>
+          <button className="btn" onClick={() => setNumber(number - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setNumber(number + 1)}>
+            +1
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseContext;
--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    cart: [{}],
+    products: [{},{}],
+    user: null,
+    //focus...
+    number:0
+}
+
+function reducer(state,action) {
+    switch(action.type) {
+        case 'add2':
+            return {...state, number: state.number+2}
+    }
+}
+
 const UseReducer = (props) => {
     return (
         <div className="UseReducer">
